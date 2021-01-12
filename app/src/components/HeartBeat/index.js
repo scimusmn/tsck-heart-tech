2a5b86c74a1a9b5b5362b842616072fd0ee05753
sanine-a <sanine.not@pm.me>
2021-01-12 11:26:54 -0600

    switch to healthy heartbeat with correct treatment

--- app/src/components/HeartBeat/index.js
+++ app/src/components/HeartBeat/index.js
@@ -1,22 +1,26 @@
 /* eslint-disable no-console */
 /* eslint-disable  prefer-destructuring */
+/* eslint-disable  no-return-assign */
 
 import React from 'react';
 import propTypes from 'prop-types';
 
 import Canvas from '@components/Canvas';
-// import avbII from '@components/HeartBeat/2d-AVB-ii';
-// import sinusBrachycardia from '@components/HeartBeat/sinus-brachycardia';
-// import sinusArrest from '@components/HeartBeat/sinus-arrest';
+import avbII from '@components/HeartBeat/2d-AVB-ii';
+import sinusBrachycardia from '@components/HeartBeat/sinus-brachycardia';
+import sinusArrest from '@components/HeartBeat/sinus-arrest';
 import sinusRhythm from '@components/HeartBeat/sinus-rhythm';
-// import vfib from '@components/HeartBeat/vfib';
-// import vtach from '@components/HeartBeat/vtach';
+import vfib from '@components/HeartBeat/vfib';
+import vtach from '@components/HeartBeat/vtach';
+
+const beep = new Audio('http://sanine.net/files/beep.mp3');
+beep.addEventListener('canplay', () => beep.playable = true);
 
 const LINE_COLOR = '#ff0000';
 const LINE_WIDTH = 5;
 const BG_COLOR = '#cccccc';
 
-function rhythm(data, heartrate, sx, sy, dx, dy, speed) {
+function rhythm(data, heartrate, sx, sy, dx, dy, speed, threshold, treatment) {
   return {
     data,
     heartrate,
@@ -24,6 +28,8 @@ function rhythm(data, heartrate, sx, sy, dx, dy, speed) {
       x: sx, y: sy, dx, dy,
     },
     speed,
+    threshold,
+    treatment,
   };
 }
 
@@ -37,32 +43,122 @@ class HeartBeat extends React.Component {
     super(props);
     this.state = {
       time: 0,
+      mode: 'playing',
     };
+
+    this.correctMessage = 'Correct. Press button to try another patient.'
+      + ' | صيح. اضغط على الزر لتجربة مريض آخر.';
+    this.incorrectMessage = 'Incorrect. Press button to try another patient.'
+      + ' | غير صحيح. اضغط على الزر لتجربة مريض آخر.';
 
     const { width, height } = this.props;
 
     this.rhythms = [
-      rhythm(sinusRhythm, 70, width * (2600 / 600), height / 2, 0, height / 4, width / 4),
+      rhythm(
+        avbII,
+        70,
+        width * (2600 / 600),
+        height / 2,
+        0,
+        height / 4,
+        width / 4,
+        0.5,
+        'pacemaker',
+      ),
+      rhythm(
+        sinusBrachycardia,
+        70,
+        width * (5000 / 600),
+        height / 2,
+        0,
+        height / 4,
+        width / 4,
+        0.5,
+        'pacemaker',
+      ),
+      rhythm(
+        sinusArrest,
+        70,
+        width * (2600 / 600),
+        height / 2,
+        0,
+        height / 4,
+        width / 4,
+        0.5,
+        'pacemaker',
+      ),
+      rhythm(
+        vfib,
+        70,
+        width * (2600 / 600),
+        height / 2,
+        0,
+        height / 4,
+        width / 4,
+        0.5,
+        'defibrillator',
+      ),
+      rhythm(
+        vtach,
+        70,
+        width * (5000 / 600),
+        height / 2,
+        0,
+        height / 4,
+        width / 4,
+        0.5,
+        'defibrillator',
+      ),
     ];
+
+    this.healthyRhythm = rhythm(
+      sinusRhythm,
+      70,
+      width * (2600 / 600),
+      height / 2,
+      0,
+      height / 4,
+      width / 4,
+      0.5,
+      'none',
+    );
 
     this.framerate = 30;
     this.delta = 1 / this.framerate;
     this.i0 = 0;
+    this.beeping = false;
 
     this.randomRhythm();
 
-    this.rhythm = this.rhythms[0];
+    this.readPacemaker = () => this.readType('pacemaker');
+    this.readDefibrillator = () => this.readType('defibrillator');
 
     this.draw = this.draw.bind(this);
-
-    console.log(sinusRhythm);
-    console.log(this.scale);
   }
 
   /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
   componentDidMount() {
     this.unpause();
+
+    document.addEventListener('keydown', (e) => {
+      switch (e.code) {
+        case 'ArrowDown':
+          this.buttonPushed();
+          break;
+
+        case 'ArrowLeft':
+          this.readPacemaker();
+          break;
+
+        case 'ArrowRight':
+          this.readDefibrillator();
+          break;
+
+        default:
+          break;
+      }
+    });
   }
 
   /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -70,12 +166,47 @@ class HeartBeat extends React.Component {
   getPoint(index) {
     const i = index % this.rhythm.data.length;
     const translation = this.rhythm.scale.x * Math.floor(index / this.rhythm.data.length);
-    console.log('i', i, 'trans', translation);
     const p = this.rhythm.data[i];
     return {
       x: this.rhythm.scale.x * p.x - this.rhythm.scale.dx + translation,
       y: this.rhythm.scale.y * p.y + this.rhythm.scale.dy,
     };
+  }
+
+  /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+  setHealthy() {
+    this.rhythm = this.healthyRhythm;
+    this.i0 = 0;
+  }
+
+  /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+  buttonPushed() {
+    const { mode } = this.state;
+    if (mode === 'playing') {
+      return;
+    }
+
+    this.randomRhythm();
+    this.setState({ time: 0, mode: 'playing' });
+    this.i0 = 0;
+  }
+
+  /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+  readType(type) {
+    const { mode } = this.state;
+    if (mode !== 'playing') {
+      return;
+    }
+
+    if (this.rhythm.treatment === type) {
+      this.setState({ mode: 'correct' });
+      this.setHealthy();
+    } else {
+      this.setState({ mode: 'incorrect' });
+    }
   }
 
   /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -95,8 +226,12 @@ class HeartBeat extends React.Component {
 
   /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
-  randomRhythm() {
-    this.rhythm = randomChoice(this.rhythms);
+  randomRhythm(allowSinus = false) {
+    let r = randomChoice(this.rhythms);
+    while (r === this.rhythm || (!allowSinus && r === sinusRhythm)) {
+      r = randomChoice(this.rhythms);
+    }
+    this.rhythm = r;
   }
 
   /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
@@ -124,6 +259,17 @@ class HeartBeat extends React.Component {
       i += 1;
     }
 
+    const y = (p.y - this.rhythm.scale.dy) / this.rhythm.scale.y;
+    console.log(y);
+    if (y < this.rhythm.threshold) {
+      if (!this.beeping && beep.playable) {
+        this.beeping = true;
+        beep.play();
+      }
+    } else {
+      this.beeping = false;
+    }
+
     ctx.stroke();
   }
 
@@ -131,7 +277,17 @@ class HeartBeat extends React.Component {
 
   render() {
     const { width, height } = this.props;
-    const { time } = this.state;
+    const { time, mode } = this.state;
+    let message;
+    if (mode !== 'playing') {
+      message = (
+        <div className="card bottom">
+          <h3>{mode === 'correct' ? this.correctMessage : this.incorrectMessage}</h3>
+        </div>
+      );
+    } else {
+      message = <span />;
+    }
     return (
       <>
         <Canvas className="behind" draw={this.draw} data={time} width={width} height={height} />
@@ -139,6 +295,7 @@ class HeartBeat extends React.Component {
           <h2>Heart Rate | معدل ضربات القلب</h2>
           <h1 className="rightAligned">{`${this.rhythm.heartrate} bpm`}</h1>
         </div>
+        {message}
       </>
     );
   }
